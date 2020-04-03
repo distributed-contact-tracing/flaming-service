bec4160dc95a9a509bf1ec8a30c26c9f9fdac967
Viktor <viktor@intrykk.se>
2020-04-03 19:35:16 +0200

    Run example functions in europe-west1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,21 @@
 import * as functions from 'firebase-functions';
 
-export const fast = functions.https.onRequest((req, res) => {
-  res.send('Fast response.');
-});
+export const fast = functions
+  .region('europe-west1')
+  .https.onRequest((req, res) => {
+    res.send('Fast response.');
+  });
 
-export const slow = functions.https.onRequest(async (req, res) => {
-  const slowResponse = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve('Slow response.'), 5000);
-    });
-  };
+export const slow = functions
+  .region('europe-west1')
+  .https.onRequest(async (req, res) => {
+    const slowResponse = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Slow response.'), 5000);
+      });
+    };
 
-  const responseText = await slowResponse();
+    const responseText = await slowResponse();
 
-  res.send(responseText);
-});
+    res.send(responseText);
+  });
