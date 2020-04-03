daffbf93a5a6e6f7b4c42497d943108c4e3e61ac
Viktor <viktor@intrykk.se>
2020-04-03 22:39:36 +0200

    Install firebase-tools before deploy

--- scripts/deploy_firebase.sh
+++ scripts/deploy_firebase.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 echo "Running script deploy_firebase.sh..."
+
+if ! [ -x "$(command -v firebase)"]; then
+  echo "Error: firebase-tools not installed."
+  exit 1
+fi
+
 firebase deploy --token $FIREBASE_TOKEN
 echo "Script deploy_firebase.sh done."
