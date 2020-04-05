3a031f3e23195775de50ecf529db1a3eec90cd03
Viktor <viktor@intrykk.se>
2020-04-05 16:53:30 +0200

    Disable unused Firebase trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,5 @@ admin.initializeApp();
 
 export { hcpAuthorizeData } from './http/hcpAuthorizeData';
 
-export { emitCreatedContactEvent } from './firestore/emitCreatedContactEvent';
+// export { emitCreatedContactEvent } from './firestore/emitCreatedContactEvent';
 export { notifyInfectedUser } from './firestore/notifyInfectedUser';
